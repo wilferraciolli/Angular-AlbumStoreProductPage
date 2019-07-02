23b2c373bc89e087dde934f9c0dff8f23ada12b8
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-07-02 14:40:47 +0100

    created album and track interfaces to map service response onto objects updated product description to use the type  of album

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,9 +12,10 @@ export class ProductService {
   constructor(private _http: Http) {
   }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
-      .map((response) => response.json());
+      .map((response) =>
+        <Album>response.json());
   }
 
 }
