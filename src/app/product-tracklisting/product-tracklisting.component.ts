f21649170464bf1d903a8c9511dbaad8927b0b4c
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-07-03 11:33:04 +0100

    Added get product list create new method to return an array of poroducts

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -16,7 +16,8 @@ export class ProductTracklistingComponent implements OnInit {
 
   ngOnInit() {
     this._productService.getAlbum(1)
-      .subscribe(response => this.albumInfo = response);
+      .subscribe(response =>
+        this.albumInfo = response);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,13 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
   }
@@ -16,6 +18,12 @@ export class ProductService {
     return this._http.get(this._albumUrl)
       .map((response) =>
         <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl)
+      .map((response) =>
+        <Product[]>response.json());
   }
 
 }
